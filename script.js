350acbe43d547dce5bd5fefdbf1ad3320e5a338f
Shayanne Samarasinghe <shayannesamarasinghe.com>
2020-05-30 12:36:12 -0400

    Found bugs in application. Cancelled shuffle function. Added for loop instead. Application complete. No bugs to reports

--- script.js
+++ script.js
@@ -1,96 +1,87 @@
-// Assignment Code
+// Variables
 var generateBtn = document.querySelector("#generate");
 var myuppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var mylowercase = "abcdefghijklmnopqrstuvwxyz";
-var mynumbers = "123456789";
+var mynumbers = "0123456789";
 var mysymbols = "!@#$%^&*";
 var passlength = 0;
 var temppass = "";
 var mypassword = "";
 
-// Write password to the #password input
+// writePassword Function
 function writePassword() {
+
   // var password = generatePassword();
   var passwordText = document.querySelector("password");
+
+  // uppercase prompt and user input
   var askuppercase = confirm("Do you want uppercase characters?");
-  if(askuppercase){
+  if (askuppercase) {
     temppass += myuppercase;
     console.log(temppass);
 
   }
-  
+
+  // lowercase prompt and user input
   var asklowercase = confirm("Do you want lowercase characters?");
-  if(asklowercase){
+  if (asklowercase) {
     temppass += mylowercase;
     console.log(temppass);
 
   }
-  
+
+  //symbol prompt and user input
   var asksymbols = confirm("Do you want symbol characters? IE:!@#$%^&* ");
-  if(asksymbols){
+  if (asksymbols) {
     temppass += mysymbols;
     console.log(temppass);
 
   }
-  
+
+  //number prompt and user input
   var asknumbers = confirm("Do you want numbers?");
-  if(asknumbers){
-    temppass += mynumbers;  
+  if (asknumbers) {
+    temppass += mynumbers;
     console.log(temppass);
-    
+
 
   }
-  
-  if(temppass.length == 0){
+
+  if (temppass.length == 0) {
     alert("You must select at least one data type. Please refresh and try again");
- 
- 
- 
+
+
+
   }
 
-  else{
-    randomtemppass = shuffle(temppass);
-    console.log(randomtemppass);
+  else {
+    // passlength prompt and user input
+    var passlength = prompt("How many characters do you want? Between 8 and 128");
 
-    var passlength = prompt ("How many characters do you want? Between 8 and 128");
     // passlength = Number(passlength);
     console.log(typeof(passlength))
-    if(passlength >= 8 && passlength <= 128 && !isNaN(passlength)){
-      var myTruncatedString = randomtemppass.substring(0,passlength);
-      document.getElementById("password").value = myTruncatedString
-
+    if (passlength >= 8 && passlength <= 128 && !isNaN(passlength)) {
+      var finalpassword = ""
+        for (i = 0; i < passlength; i++) {
+        finalpassword += temppass.charAt(Math.floor(Math.random() * temppass.length))
+      }
       
+      document.getElementById("password").value = finalpassword
 
 
     }
 
-
-    else{
+    else {
       alert("Input not correct. Please start over");
     }
+
+
   }
 
 
-  
 
-
-
-  
-
-   
 
 }
 
-function shuffle(s) {
-  var arr = s.split('');          
-  
-  arr.sort(function() {
-    return 0.5 - Math.random();
-  });  
-  s = arr.join('');                
-  return s;  
-}                      
-
-
-// // Add event listener to generate button
+//  Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
